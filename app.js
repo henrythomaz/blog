b3a32f5252912f7ce8afe167cc868ba0c4460d87
Henry <henrytoliveriacampos@gmail.com>
2025-02-25 23:05:26 -0300

    começando o estilo

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-// ROUTES
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.use(express.static("public"));
+// ROTAS
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 })
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server runNig on port ${PORT}`);
+    console.log(`rodando na porta: ${PORT}`);
 });
